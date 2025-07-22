d0ca9b95cc7d5d6d707117b72d7d53a79a7256c0
ronnkarani <ronnkarani25@gmail.com>
2025-07-22 19:45:11 +0300

    render settings

--- rental_site/settings.py
+++ rental_site/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7p)=_j8f!itq%kw2&+_qz(q)#b^ta_)0tb%25jbiep6w^47)+t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['moovin.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
